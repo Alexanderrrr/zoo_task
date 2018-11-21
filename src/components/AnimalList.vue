d69eb6e9e8e3a0db7f76d8a89771033a5d1fd7a9
Alexanderrrr <acons1987@gmail.com>
2018-11-21 02:01:29 +0100

    Zadatak 1

---
+++ src/components/AnimalList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th>Index</th>
+          <th>Species</th>
+          <th>Name</th>
+          <th>Birth</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(animal,index) in animals" :key="index">
+          <th>{{ index + 1 }}</th>
+          <td>{{ animal.species }}</td>
+          <td>{{ animal.name }}</td>
+          <td>{{ animal.birth  }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      animals: [
+        {species: "mammals", name: "dog", birth: "01.01.1990"},
+        {species: "mammals", name: "horse", birth: "01.01.2000"},
+        {species: "amphibians", name: "frog", birth: ""},
+        {species: "reptiles", name: "snake", birth: ""},
+        {species: "reptiles", name: "aligator", birth: "01.01.1800"},
+      ],
+    };
+  }
+}
+</script>
+
+<style lang="css">
+</style>
